abcf920ad0241b3bc5e086a4d1f28a7c5262d12f
Hiba Moideen <hiba.moideen@thefirstgroup.com>
2022-10-06 16:11:49 +0400

    changes

---
+++ js/soluna.js
@@ -0,0 +1,42 @@
+// Navbar changes when the user scrolls down 80px from the top of the document
+global.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").classList.add("navbar-scroll");
+  } else {
+    document.getElementById("navbar").classList.remove("navbar-scroll");
+  }
+
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+     document.getElementById("nav-tabs").classList.add("nav-tabs-scroll");
+   } else {
+     document.getElementById("nav-tabs").classList.remove("nav-tabs-scroll");
+   }
+
+}
+
+
+//slick carousel
+{/* <script src="jquery-3.5.1.js" type="text/javascript">
+
+$(document).ready(function(){
+  $('.carouselToShow').slick({
+    centerMode: true,
+    centerPadding: '20%',
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+  $('.carouselPlain').slick({
+    autoplay: false,
+    autoplaySpeed: 2000,
+  }); 
+  
+  }); */}
+
+
+
+// </script> 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/soluna.css"
+import {React, useEffect} from 'react'
+//import "../js/jquery-3.2.1.slim.min.js"
+import "../styles/bootstrap.min.css"
+//import 'bootstrap/dist/css/bootstrap.css';
+import Head from 'next/head'
+
+import "../styles/soluna.css"
+//import "../js/wow.min.js"
+//import "../js/masonry.pkgd.min.js"
+
+import "../js/soluna.js"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+//   useEffect(()=>{
+//     import("bootstrap/dist/js/bootstrap");
+// },[])
+  return (<>
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </Head>
+    
+    <Component {...pageProps} />
+    </>)
 }
 
 export default MyApp
