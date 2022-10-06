41f30b38c1502fe49afbe02cc6306a921c276436
Hiba Moideen <hiba.moideen@thefirstgroup.com>
2022-10-06 16:11:26 +0400

    updates

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
-
-const webpack = require("webpack");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(
-     new webpack.ProvidePlugin({
-     $: "jquery",
-     jQuery: "jquery",
-     "window.jQuery": "jquery",
-  }));
- return config;
-},
-};
-
-module.exports = {
-  nextConfig,
-  images: {
-    domains: ['localhost'],
-  },
-  future: { webpack5: true }
 }
+
+module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,7 @@
-import "../styles/soluna.css"
-import {React, useEffect} from 'react'
-//import "../js/jquery-3.2.1.slim.min.js"
-import "../styles/bootstrap.min.css"
-//import 'bootstrap/dist/css/bootstrap.css';
-import Head from 'next/head'
-
-import "../styles/soluna.css"
-//import "../js/wow.min.js"
-//import "../js/masonry.pkgd.min.js"
-
-import "../js/soluna.js"
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-//   useEffect(()=>{
-//     import("bootstrap/dist/js/bootstrap");
-// },[])
-  return (<>
-    <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </Head>
-    
-    <Component {...pageProps} />
-    </>)
+  return <Component {...pageProps} />
 }
 
 export default MyApp
